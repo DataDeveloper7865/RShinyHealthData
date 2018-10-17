2ba52ab8c3cdf40a4a569e946797bf2ae65804b2
DataDeveloper7865 <Whelan.stephen2016@gmail.com>
2018-10-16 22:39:46 -0700

    Modified to include Death Data, to present Histo

--- DevelopmentIntelligencePlatform.R
+++ DevelopmentIntelligencePlatform.R
@@ -2,59 +2,47 @@ library(shiny)
 
 
 ##insert data file here
-myData <- read.csv("C:/Users/Stephen/Desktop/R Projects/practice datasets/breslow.csv", header = T)
+myData <- read.csv("C:/Users/Stephen/Desktop/R Projects/practice datasets/NCHS_-_Leading_Causes_of_Death__United_States.csv", header = T)
 
-
-ui <- 
-  fluidPage("Development Intelligence Platform",
-    tabPanel("icIEF Data",
+#Define UI for app that draws a histogram
+ ui <- fluidPage(
+                
+    titlePanel("Health Data"),
+             
       sidebarLayout(
+        
         sidebarPanel(
-      ###add input functions here
-        checkboxGroupInput(inputId = "molecules", label = "select molecules to view", choices = myData$Mol)),
+          ###add input functions here
+          checkboxGroupInput(inputId = "year", label = "select year to view", choices = myData$Year)
+          
+          ),
   
+        
+        #main panel for displaying outputs
         mainPanel(
-        ####add output functions here
-          plotOutput(outputId = "boxplotIEF"),
-          textOutput(outputId = "text")
+          
+          ####add output functions here
+          plotOutput(outputId = "DeathsHisto"),
+          textOutput(outputId = "YearsSelected")
         )
       )
-    ),
-    tabPanel("Summary",
-      verbatimTextOutput("summary")
     )
-  )
-
-
-
-
+  
 
 ### 1)if building an output object, save that output object to output$ ....... eg. output$hist
 ### 2)build objects to display with render*()   eg... renderPlot(), etc.
 ### 3)Use input values with input$ eg.... input$molecules
 server <- function(input, output) {
 
-    output$boxplotIEF <- renderPlot({ 
-      ggplot(myData, aes(myData$icIEFtype, myData$icIEF)) +
-        geom_boxplot() +
-        geom_jitter(aes(myData$icIEFtype, myData$icIEF),
-                    position = position_jitter(width = 0.1, height = 0),
-                    alpha = 0.6,
-                    size = 3,
-                    show.legend = FALSE
-        ) +
-        xlab("Type of Species") +
-        ylab("Relative % of Species")+
-        title("icIEF Data for Historical Development Molecules")
+    output$DeathsHisto <- renderPlot({ 
+      
+      hist(myData$Deaths)
+      
     })
+  
     
-    
-    output$summary <- renderPrint({
-      summary(myData)
-    })
-    
-    output$text <- renderText({
-        input$molecules
+    output$YearsSelected <- renderText({
+        input$year
       })
 
 }
